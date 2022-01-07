dabe832e54f876cb1c4c78dc65cdd7177d1abbe7
naveenveera789 <naveenveera789@gmail.com>
2022-01-07 20:02:54 +0530

    [naveenveera789] add : The Daily Employee Wage function using for loop program is done

---
+++ EmpForLoop.js
@@ -0,0 +1,29 @@
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HOURS=4;
+const FULL_TIME_HOURS=8;
+const WAGE_PER_HOUR=20;
+const NUM_OF_WORKING_DAYS=20;
+
+function getWorkingHours(empCheck)
+{
+      switch(empCheck)
+      {
+            case IS_PART_TIME:
+               return PART_TIME_HOURS;
+            case IS_FULL_TIME:
+               return FULL_TIME_HOURS;
+            default:
+               return 0;
+      }
+}
+
+let totalEmpHrs = 0;
+ for(let day = 1;day <= NUM_OF_WORKING_DAYS;day++)
+ {
+    let empCheck=Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs += getWorkingHours(empCheck);
+    console.log("The Total Working Hours of Employee by Day " + day + " is : " + totalEmpHrs);
+ }
+ let empWage = totalEmpHrs * WAGE_PER_HOUR;
+ console.log("Total Working Hours: " + totalEmpHrs + " Total Emp Wage: " + empWage);
